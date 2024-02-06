8c861b08156e398abc3799c3babcb451482f60f9
Noi Vinsmoke <thana.drnn@hotmail.com>
2024-02-07 03:02:21 +0700

    Revert Blog.tsx

--- src/components/blog-pages/Blog.tsx
+++ src/components/blog-pages/Blog.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import FacebookIcon from '@mui/icons-material/Facebook';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import XIcon from '@mui/icons-material/X';
 import Header from './Header';
 import MainFeaturedPost from './MainFeaturedPost';
@@ -13,6 +15,19 @@ import Footer from './Footer';
 import post1 from './blog-post.1.md';
 import post2 from './blog-post.2.md';
 import post3 from './blog-post.3.md';
+
+const sections = [
+  { title: 'Technology', url: '#' },
+  { title: 'Design', url: '#' },
+  { title: 'Culture', url: '#' },
+  { title: 'Business', url: '#' },
+  { title: 'Politics', url: '#' },
+  { title: 'Opinion', url: '#' },
+  { title: 'Science', url: '#' },
+  { title: 'Health', url: '#' },
+  { title: 'Style', url: '#' },
+  { title: 'Travel', url: '#' },
+];
 
 const mainFeaturedPost = {
   title: 'Title of a longer featured blog post',
@@ -68,9 +83,15 @@ const sidebar = {
   ],
 };
 
+// TODO remove, this demo shouldn't need to reset the theme.
+// const defaultTheme = createTheme();
+
 export default function Blog() {
   return (
+    // theme={defaultTheme}
+    // <ThemeProvider>
     <>
+      <CssBaseline />
       <Container maxWidth="lg">
         <Header title="Blog" />
         <main>
@@ -96,5 +117,6 @@ export default function Blog() {
         description="Something here to give the footer a purpose!"
       />
     </>
+    // </ThemeProvider>
   );
 }
